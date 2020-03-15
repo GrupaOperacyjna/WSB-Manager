591d2f0b94fafa2224562f888289781075894ae4
Kinga <kinga_r4@wp.pl>
2020-03-15 12:39:36 +0100

    Class authors

--- src/WSBManager/Authors.java
+++ src/WSBManager/Authors.java
@@ -4,5 +4,42 @@
 package WSBManager;
 
 public class Authors {
+	private int born;
+	private String name;
+	private String surname;
 
+	public int getBorn() {
+		return born;
+	}
+
+	public void setBorn(int born) {
+		this.born = born;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	// Konstruktor domyslny - tamten bez argumentów by³ wywo³ywany domyœlnie
+	public Authors(int born, String name, String surname) {
+		this.born=born;
+		this.name=name;
+		this.surname=surname;
+	}
+
+	public String toStringoverride() {
+		return "[" + born + "]-" + name + "-" + surname;
+	}
 }
