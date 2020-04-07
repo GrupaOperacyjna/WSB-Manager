c85f2b8827e07e333b25aa9095486f3998d83c27
patryk8246 <62201968+patryk8246@users.noreply.github.com>
2020-04-07 02:32:21 +0200

    Add files via upload
    Klasa zapisująca dane w pliku.csv

--- src/WSBManager/ExportToCSV.java
+++ src/WSBManager/ExportToCSV.java
@@ -1,5 +1,34 @@
 package WSBManager;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 public class ExportToCSV {
 
+		static String output;
+		static List<List<String>> data = Arrays.asList();
+		
+		public void Export(String output, List data) {
+			this.output = output;
+			this.data = data;
+		}
+		
+		
+		public static void exportToCSV() throws IOException {
+			try {
+			FileWriter csvWriter = new FileWriter(output);
+
+			for (List<String> rowData : data) {
+			    csvWriter.append(String.join(",", rowData));
+			    csvWriter.append("\n");
+			}
+
+			csvWriter.flush();
+			csvWriter.close();	
+		} catch (IOException ex) {
+            System.out.println("Problem z dostêpem do pliku");
+        		}
+	}
 }
