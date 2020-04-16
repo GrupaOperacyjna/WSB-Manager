5568787b548a3e1efd797bf7acf9904011f480cb
patryk8246 <p.karczewski1995@gmail.com>
2020-04-17 01:15:13 +0200

    Dodanie metody checkIsbn(String bookIsbnToCheck)

--- src/WSBManager/Books.java
+++ src/WSBManager/Books.java
@@ -5,7 +5,70 @@
 package WSBManager;
 
 public class Books {
-	 class Date {
+	public class Zamiana {
+		 
+		 public int zamianZcharNaInt(int liczba3) {
+			 int liczba4 = 0;
+			 switch(liczba3) {
+			 case 48:
+			 {
+				liczba4 = 0;
+				break;
+			 }
+			 case 49:
+			 {
+				liczba4 = 1;
+				break;
+			 }
+			 case 50:
+			 {
+				liczba4 = 2;
+				break;
+			 }
+			 case 51:
+			 {
+				liczba4 = 3;
+				break;
+			 }
+			 case 52:
+			 {
+				liczba4 = 4;
+				break;
+			 }
+			 case 53:
+			 {
+				liczba4 = 5;
+				break;
+			 }
+			 case 54:
+			 {
+				liczba4 = 6;
+				break;
+			 }
+			 case 55:
+			 {
+				liczba4 = 7;
+				break;
+			 }
+			 case 56:
+			 {
+				liczba4 = 8;
+				break;
+			 }
+			 case 57:
+			 {
+				liczba4 = 9;
+				break;
+			 }
+				 
+			 }
+			 return liczba4;
+		 }
+		 
+	 }
+	Zamiana p = new Zamiana();
+	
+	class Date {
 		 
 		 private int day, month, year;
 		 public	 void setDate(int day, int month, int year) {
@@ -18,8 +81,7 @@ public class Books {
 	 		 }
 	 }
 	 
-	 private String bookName, bookCover, bookType;
-	 private int bookIsbn; 
+	 private String bookName, bookCover, bookType, bookIsbn;
 	 private Date bookRelaseDate= new Date();
 	 
 	 public void setBookName(String bookName) {
@@ -42,10 +104,36 @@ public class Books {
 	 public String getBookCover() {
 			 return bookCover;
 		 }
-	 public void setBookIsbn(int bookIsbn) {   
+	 public void setBookIsbn(String bookIsbn) {   
 		 this.bookIsbn = bookIsbn;
 	 	 }
-	 public int getBookIsbn() {
+	 public boolean checkIsbn(String bookIsbnToCheck) {
+		 int prefiksEANspr1 = p.zamianZcharNaInt(bookIsbnToCheck.charAt(0));
+		 int prefiksEANspr2 = p.zamianZcharNaInt(bookIsbnToCheck.charAt(1));
+		 int prefiksEANspr3 = p.zamianZcharNaInt(bookIsbnToCheck.charAt(2));
+		 int dlugoscIsbn = bookIsbnToCheck.length();
+		 
+		 int reszta_z_dzielenia = (p.zamianZcharNaInt(bookIsbnToCheck.charAt(0))*1)+(p.zamianZcharNaInt(bookIsbnToCheck.charAt(1))*3)+(p.zamianZcharNaInt(bookIsbnToCheck.charAt(2))*1)+
+				 (p.zamianZcharNaInt(bookIsbnToCheck.charAt(3))*3)+
+				 (p.zamianZcharNaInt(bookIsbnToCheck.charAt(4))*1)+
+				 (p.zamianZcharNaInt(bookIsbnToCheck.charAt(5))*3)+
+				 (p.zamianZcharNaInt(bookIsbnToCheck.charAt(6))*1)+
+				 (p.zamianZcharNaInt(bookIsbnToCheck.charAt(7))*3)+
+				 (p.zamianZcharNaInt(bookIsbnToCheck.charAt(8))*1)+
+				 (p.zamianZcharNaInt(bookIsbnToCheck.charAt(9))*3)+
+				 (p.zamianZcharNaInt(bookIsbnToCheck.charAt(10))*1)+
+				 (p.zamianZcharNaInt(bookIsbnToCheck.charAt(11))*3);
+		 int reszta = reszta_z_dzielenia%10;
+		 int znakKontrolny = 10 - reszta;
+		 if (znakKontrolny == 10) {
+			 znakKontrolny = 0;
+		 }
+		
+		return (dlugoscIsbn == 13 && p.zamianZcharNaInt(bookIsbnToCheck.charAt(12))==znakKontrolny && prefiksEANspr1 == 9 && prefiksEANspr2 == 7 && prefiksEANspr3 == 8 || prefiksEANspr3 == 9);
+		
+	 }
+	
+	 public String getBookIsbn() {
 			 return bookIsbn;
 		 }
 	 public void setBookRelaseDate(int day, int month, int year) {   
@@ -54,9 +142,8 @@ public class Books {
 	 public String getBookRelaseDate() {
 			 return bookRelaseDate.getDate();
 		 }
-	 public String toString(int zamiana) {
-		 String zamiana2;
-		 zamiana2 = (zamiana2+zamiana);
-		 return zamiana2;
+	 @Override
+	 public String toString() {
+		return bookName+" "+bookCover+" "+bookIsbn+" "+bookType;
 	 }
 }
